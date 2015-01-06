7aa80e621eb316db9846a4f2a96faffc29b8ce14
elevicky <ericlevicky@gmail.com>
2015-01-05 22:30:45 -0500

    first promote

---
+++ LinqPlayground/Queries.cs
@@ -0,0 +1,79 @@
+﻿using LinqPlayground.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPlayground
+{
+    public class Queries
+    {
+        public Queries()
+        {
+
+        }
+
+        public static bool IsDatabaseConnected()
+        {
+            using (var context = new AdventureWorksEntities())
+            {
+                DbConnection connection = context.Database.Connection;
+                try
+                {
+                    connection.Open();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static List<Person> GetPeople()
+        {
+            List<Person> allPeople = new List<Person>();
+
+            using (var context = new AdventureWorksEntities())
+            {
+                allPeople = context.People.ToList<Person>();
+            }
+
+            return allPeople;
+        }
+
+        public static List<Person> GetPeopleWithFirstName(string nameToMatch)
+        {
+            List<Person> peopleWithFirstName = new List<Person>();
+            using (var context = new AdventureWorksEntities())
+            {
+                peopleWithFirstName = context.People.Where(p => p.FirstName == nameToMatch).ToList<Person>();
+            }
+            return peopleWithFirstName;
+        }
+
+        public static long GetNumPeople()
+        {
+            long numPeople = 0L;
+            using (var context = new AdventureWorksEntities())
+            {
+                numPeople = context.People.Count();
+            }
+            return numPeople;
+        }
+
+        public static long GetNumPeopleWithFirstName(string nameToMatch)
+        {
+            long numPeople = 0L;
+            using (var context = new AdventureWorksEntities())
+            {
+                numPeople = context.People.Count(p => p.FirstName == nameToMatch);
+            }
+            return numPeople;
+        }
+    }
+}
---
+++ LinqPlaygroundTests/AdventureWorksTests.cs
@@ -0,0 +1,69 @@
+﻿using LinqPlayground.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.EntityClient;
+using System.Data.Linq;
+using System.Data.Objects;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace LinqPlaygroundTests
+{
+    [TestClass]
+    public class AdventureWorksTests
+    {
+        [TestMethod]
+        public void IsDatabaseConnected()
+        {
+            bool isDatabaseConnected = LinqPlayground.Queries.IsDatabaseConnected();
+            Assert.IsTrue(isDatabaseConnected);
+        }
+
+        [TestMethod]
+        public void AreTherePeople()
+        {
+            List<Person> people = LinqPlayground.Queries.GetPeople();
+            int numPeople = people.Count;
+            Assert.IsTrue(numPeople > 0);
+        }
+
+        [TestMethod]
+        public void GetAllErics()
+        {
+            List<Person> allErics = LinqPlayground.Queries.GetPeopleWithFirstName("Eric");
+            int actualCountErics = allErics.Count;
+            int expectedCountErics = 63;
+            Assert.AreEqual(actualCountErics, expectedCountErics);
+        }
+
+        [TestMethod]
+        public void GetAllAlyssas()
+        {
+            List<Person> allErics = LinqPlayground.Queries.GetPeopleWithFirstName("Alyssa");
+            int actualCountErics = allErics.Count;
+            int expectedCountErics = 67;
+            Assert.AreEqual(actualCountErics, expectedCountErics);
+        }
+
+        [TestMethod]
+        public void GetCountErics()
+        {
+            long numErics = LinqPlayground.Queries.GetNumPeopleWithFirstName("Eric");
+            long expectedCountErics = 63;
+            Assert.AreEqual(numErics, expectedCountErics);
+        }
+
+        [TestMethod]
+        public void GetCountAlyssas()
+        {
+            long numAlyssas = LinqPlayground.Queries.GetNumPeopleWithFirstName("Alyssa");
+            long expectedCountAlyssas = 67;
+            Assert.AreEqual(numAlyssas, expectedCountAlyssas);
+        }
+    }
+}
